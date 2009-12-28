f2d5a72925268c8247758bb295bfdcf81edb77e4
Jacob Friis Saxberg <jacob@jayBook.local>
2009-12-28 23:50:09 +0100

    json fix + cleanup + handle multiple from & to parameters

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -11,40 +11,79 @@ Sammy.debug = true;
 });*/
 
 GET('/', function () {
-	return template('index.html');
+	//return template('index.html');
+	return redirect('/index.html');
 });
 
 GET('/convert', function () {
 	if (this.request.query['from'] || this.request.query['to']) {
+		var result = [];
+		// convert to URL?
 		if (this.request.query['from']) {
-			var result = urlFromGraphNode(this.request.query['from']);
-		} else if (this.request.query['to']) {
-			var result = urlToGraphNode(this.request.query['to']);
+			var from = this.request.query['from'];
+			if (isArray(from)) {
+				for (var i in from) {
+					result.push(urlFromGraphNode(from[i]));
+				}
+			} else {
+				result.push(urlFromGraphNode(from));
+			}
 		}
-		this.response.mime = 'application/json';
-		return '[' + result + ']';
+		// convert to SGN?
+		if (this.request.query['to']) {
+			var to = this.request.query['to'];
+			if (isArray(to)) {
+				for (var i in to) {
+					result.push(urlToGraphNode(to[i]));
+				}
+			} else {
+				result.push(urlToGraphNode(to));
+			}
+		}
+		// any callback?
+		var callback = false;
+		if (this.request.query['callback'] || this.request.query['jsoncallback']) {
+			callback = (this.request.query['callback']) ? this.request.query['callback'] : this.request.query['jsoncallback'];
+			this.response.mime = 'text/javascript';
+		} else {
+			this.response.mime = 'application/json';
+		}
+		// prepare response
+		result = JSON.stringify(result);
+		if (callback != false) {
+			result = callback + '(' + result + ');';
+		}
+		// done
+		return result;
 	}
-	return 'I need a from or to parameter.';
+	// error
+	return JSON.stringify({error: 'I need a from or to parameter.'});
 });
 
-//function main (aRequest) {}
+function isArray (obj) {
+	return obj.constructor == Array;
+}
 
 function urlToGraphNode (url) {
 	var output = {};
-	output[url] = nodemapper.urlToGraphNode(url);
-	return JSON.stringify(output);
+	output[url] = {};
+	var result = nodemapper.urlToGraphNode(url);
+	var type = (result.indexOf('://') > 0) ? result.substr(0, result.indexOf('://')).toLowerCase() : '?';
+	output[url][type] = result;
+	return output;
 }
 
 function urlFromGraphNode (url) {
 	var types = ["profile", "content", "atom", "rss", "blog", "openid", "foaf", "addfriend"];
 	var output = {};
+	output[url] = {};
 	for (var typeIdx in types) {
 		var link = nodemapper.urlFromGraphNode(url, types[typeIdx]);
 		if (!link) {
-			output[types[typeIdx]] = 'none';
+			output[url][types[typeIdx]] = 'none';
 		} else {
-			output[types[typeIdx]] = link;
+			output[url][types[typeIdx]] = link;
 		}
 	}
-	return JSON.stringify(output);
+	return output;
 }
