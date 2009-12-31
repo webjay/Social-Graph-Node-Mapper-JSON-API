5e8166cb75d5306b42dd6f85494dbb013f06d8b9
Jacob Friis Saxberg <jacob@jayBook.local>
2009-12-31 14:03:15 +0100

    bootstrap.js missing?

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,7 +3,7 @@ system.use('com.joyent.Sammy');
 system.use('org.json.json2');
 system.use('nodemapper-base');
 
-//Sammy.debug = true;
+Sammy.debug = true;
 // uneval(this);
 
 GET('/', function () {
