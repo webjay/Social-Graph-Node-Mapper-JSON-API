6e7ca4f922cec6eb5cd8e30a520c3347f61ee1d2
Jacob Friis Saxberg <jacob@jayBook.local>
2010-01-01 16:05:22 +0100

    Simplified parameters to just url.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,9 +3,6 @@ system.use('com.joyent.Sammy');
 system.use('org.json.json2');
 system.use('convert');
 
-//Sammy.debug = true;
-// uneval(this);
-
 GET('/', function () {
 	return redirect('/index.html');
 });
--- js/convert.js
+++ js/convert.js
@@ -1,32 +1,20 @@
 
 GET('/convert', function () {
-	if (this.request.query['from'] || this.request.query['from[]'] || this.request.query['to'] || this.request.query['to[]']) {
-		sitesLoad();
-		var result = {};
-		// convert from SGN?
-		if (this.request.query['from'] || this.request.query['from[]']) {
-			var from = (this.request.query['from[]']) ? this.request.query['from[]'] : this.request.query['from'];
-			if (!isArray(from)) {
-				from = [from];
-			}
-			for (var i in from) {
-				var url = from[i];
-				result[url] = urlFromGraphNode(url);
-			}
+	if (this.request.query['url[]'] || this.request.query['url']) {
+		// start engine
+		nodemapperLoad();
+		// set vars
+		var urls = (this.request.query['url[]']) ? this.request.query['url[]'] : this.request.query['url'];
+		if (urls.constructor != Array) {
+			urls = [urls];
 		}
-		// convert to SGN?
-		if (this.request.query['to'] || this.request.query['to[]']) {
-			var to = (this.request.query['to[]']) ? this.request.query['to[]'] : this.request.query['to'];
-			if (!isArray(to)) {
-				to = [to];
-			}
-			for (var i in to) {
-				var url = to[i];
-				if (nodemapper.HTTP_REGEX.exec(url)) {
-					result[url] = urlToGraphNode(url);
-				} else {
-					result[url] = urlToGraphNodeNotHTTP(url);
-				}
+		var result = {};
+		// convert
+		for (var i in urls) {
+			if (nodemapper.SGN_REGEX.exec(urls[i])) { /* if SGN */
+				result[urls[i]] = urlFromGraphNode(urls[i]);
+			} else { /* HTTP or non-HTTP, handled by nodemapper.urlToGraphNode */
+				result[urls[i]] = urlToGraphNode(urls[i]);
 			}
 		}
 		// any callback?
@@ -52,14 +40,14 @@ GET('/convert', function () {
 	// error
 	this.response.mime = 'text/plain';
 	this.response.code = 415;
-	return 'Error 415: I need a from or to parameter.';
+	return 'Error 415: I need an url parameter.';
 });
 
-function sitesLoad () {
-	// get nodemapper-base
+function nodemapperLoad () {
+	// load nodemapper-base
 	var file = system.filesystem.get('google-sgnodemapper-read-only/nodemapper-base.js');
 	eval(file.contents);
-	// get sites
+	// load sites
 	var sites = ['amazon', 'aol', 'blogspot', 'facebook', 'flickr', 'friendfeed', 'google', 'hi5', 'lastfm', 'livejournal', 'meetup', 'mybloglog', 'myspace', 'nonhttp', 'opera', 'russia', 'sapo', 'simple', 'spin-de', 'stumbleupon', 'threadless', 'tribe', 'twitter', 'wakoopa', 'wordpress', 'yelp', 'zooomr'];
 	for (var i in sites) {
 		var file = system.filesystem.get('google-sgnodemapper-read-only/sites/' + sites[i] + '.js');
@@ -72,14 +60,10 @@ function sitesLoad () {
 	}	
 }
 
-function isArray (obj) {
-	return obj.constructor == Array;
-}
-
 function urlToGraphNode (url) {
 	var output = {};
 	var result = nodemapper.urlToGraphNode(url);
-	var type = (result.indexOf('://') > 0) ? result.substr(0, result.indexOf('://')).toLowerCase() : '';
+	var type = (result.indexOf(':') > 0) ? result.substr(0, result.indexOf(':')).toLowerCase() : '';
 	output[type] = result;
 	return output;
 }
@@ -95,13 +79,5 @@ function urlFromGraphNode (url) {
 			output[types[typeIdx]] = link;
 		}
 	}
-	return output;
-}
-
-function urlToGraphNodeNotHTTP (url) {
-	var output = {};
-	var result = nodemapper.urlToGraphNodeNotHTTP(url);
-	var type = (result.indexOf(':') > 0) ? result.substr(0, result.indexOf(':')).toLowerCase() : '';
-	output[type] = result;
 	return output;
 }
